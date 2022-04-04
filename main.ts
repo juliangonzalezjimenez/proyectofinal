8fb2003ce2329b8be451516ccffc579950a2d5a6
juliangonzalezjimenez <98742865+juliangonzalezjimenez@users.noreply.github.com>
2022-04-04 11:29:37 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,30 @@
-input.onButtonPressed(Button.A, function () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        maqueen.motorStop(maqueen.Motors.All)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+    }
 })
-input.onButtonPressed(Button.B, function () {
-    maqueen.motorStop(maqueen.Motors.All)
-})
+radio.setGroup(21)
 basic.showString("Hola")
 music.playMelody("C5 B A G F E D C ", 120)
 basic.forever(function () {
-	
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    basic.pause(50)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+})
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 6) {
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.pause(100)
+    }
 })
